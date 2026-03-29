c18f79f7fcc88c676c1d7e2755986a89540c8545
Anna Rescifina <ar8278a@american.edu>
2026-03-29 13:33:37 -0400

    added name to app

---
+++ shiny_app_rescifina.R
@@ -0,0 +1,37 @@
+# Anna Daria Rescifina
+
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Anna Rescifina"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("n", "Number of observations:", 200),
+      
+      selectInput("plot_type", "Choose plot type:",
+                  choices = c("Histogram", "Boxplot")),
+      
+      selectInput("color", "Choose color (for boxplot):",
+                  choices = c("red", "blue", "green", "gray"))
+    ),
+    
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    data <- runif(input$n)
+    
+    if (input$plot_type == "Histogram") {
+      hist(data)
+    } else {
+      boxplot(data, col = input$color)
+    }
+  })
+}
+
+shinyApp(ui = ui, server = server)
